b0cf666c3e46fba1a0ca88ded80a2a4afd24081b
glaubergoncalves <sistemas.glauber@gmail.com>
2020-04-14 23:16:30 -0300

    Updates

---
+++ precache-manifest.f04f7f956eee08a4062bc7bdf84da1ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "540552cc510dd40d672dc5a1e65af58d",
+    "url": "/react-netflix-frontend/index.html"
+  },
+  {
+    "revision": "05f9f3cb08be60785f95",
+    "url": "/react-netflix-frontend/static/css/main.091c6853.chunk.css"
+  },
+  {
+    "revision": "0fb6e6404fdd18f9c367",
+    "url": "/react-netflix-frontend/static/js/2.d27ed35c.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/react-netflix-frontend/static/js/2.d27ed35c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05f9f3cb08be60785f95",
+    "url": "/react-netflix-frontend/static/js/main.ce0f85b8.chunk.js"
+  },
+  {
+    "revision": "694f8d2d76a71ef9d64c",
+    "url": "/react-netflix-frontend/static/js/runtime-main.ef570a35.js"
+  },
+  {
+    "revision": "dc2411c98b969c2d71b0920ae3e2d9d8",
+    "url": "/react-netflix-frontend/static/media/bg.dc2411c9.jpg"
+  },
+  {
+    "revision": "35f3a5aec6a2026d1f96de322219136d",
+    "url": "/react-netflix-frontend/static/media/logo.35f3a5ae.svg"
+  }
+]);
